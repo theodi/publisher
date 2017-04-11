227a95932266038ca2e1c9cc4f580adf6f24a195
James Smith <james@floppy.org.uk>
2017-04-11 10:24:44 +0100

    make URLs https by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include GDS::SSO::ControllerMethods
+  
+  def default_url_options(options={})
+    options.merge :protocol => "https"
+  end
+  
 end
