0fdbc7da215d72b7cf7fb5f85dddb54bc3c7e665
James Smith <james@floppy.org.uk>
2017-04-11 14:07:38 +0100

    Revert "make URLs https by default"
    This reverts commit 227a95932266038ca2e1c9cc4f580adf6f24a195.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include GDS::SSO::ControllerMethods
-  
-  def default_url_options(options={})
-    options.merge :protocol => "https"
-  end
-  
 end
